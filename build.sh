ab9a6d631e0daa5bc6717c6a0ec4b02feefc3335
Hendrik Niemeyer <hendrik.niemeyer@gmail.com>
2021-12-02 13:24:02 +0100

    make list of sanitizers

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-set -euxo pipefail
+echo "Address Sanitizer"
 cmake -S . -B ./build -GNinja -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_COMPILER=g++-10 -DENABLE_SANITIZER_ADDRESS=ON
 cmake --build ./build -v
 ./build/src/invalid-index
+sleep 10
+rm -rf buid
+echo "--------------------------------------------"
