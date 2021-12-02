f4b4cacc84d25efab341fdad96f5acf97925453f
Hendrik Niemeyer <hendrik.niemeyer@gmail.com>
2021-12-02 13:40:37 +0100

    Thread sanitizer

---
+++ build_thread.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Thread Sanitizer"
+cmake -S . -B ./build -GNinja -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_COMPILER=g++-10 -DENABLE_SANITIZER_THREAD=ON
+cmake --build ./build -v
+./build/src/invalid-index
+rm -rf build/
