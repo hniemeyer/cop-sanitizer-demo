0d79f6d74dd4724570c8a9903a01109aaf1d60cb
Hendrik Niemeyer <hendrik.niemeyer@gmail.com>
2021-12-02 13:36:44 +0100

    remove sleep

--- build_memory.sh
+++ build_memory.sh
@@ -3,6 +3,4 @@ echo "Memory Sanitizer"
 cmake -S . -B ./build -GNinja -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_COMPILER=g++-10 -DENABLE_SANITIZER_MEMORY=ON
 cmake --build ./build -v
 ./build/src/invalid-index
-sleep 10
 rm -rf build/
-echo "--------------------------------------------"
