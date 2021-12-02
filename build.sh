f4f2b20079fcdc7ea12a9f398c1942d8f8f446c9
Hendrik Niemeyer <hendrik.niemeyer@gmail.com>
2021-12-02 13:34:29 +0100

    run address,leak and undefined sanitizer at the same time

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Address Sanitizer"
-cmake -S . -B ./build -GNinja -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_COMPILER=g++-10 -DENABLE_SANITIZER_ADDRESS=ON
-cmake --build ./build -v
-./build/src/invalid-index
-sleep 10
-rm -rf buid
-echo "--------------------------------------------"
