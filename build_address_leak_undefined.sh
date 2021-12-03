4828379a77f2fce6eac0f5157e5c27df32faa3af
Hendrik Niemeyer <hendrik.niemeyer@gmail.com>
2021-12-03 07:04:17 +0100

    Remove execution and deletion from bash files

--- build_address_leak_undefined.sh
+++ build_address_leak_undefined.sh
@@ -3,5 +3,3 @@ echo "Address, Leak and Undefined Behavior Sanitizer"
 cmake -S . -B ./build -GNinja -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_COMPILER=g++-10 -DENABLE_SANITIZER_ADDRESS=ON \
  -DENABLE_SANITIZER_LEAK=ON -DENABLE_SANITIZER_UNDEFINED_BEHAVIOR=ON 
 cmake --build ./build -v
-./build/src/invalid-index
-rm -rf build/
--- build_thread.sh
+++ build_thread.sh
@@ -2,5 +2,3 @@
 echo "Thread Sanitizer"
 cmake -S . -B ./build -GNinja -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_COMPILER=g++-10 -DENABLE_SANITIZER_THREAD=ON
 cmake --build ./build -v
-./build/src/invalid-index
-rm -rf build/
