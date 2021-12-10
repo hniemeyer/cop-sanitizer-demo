6d174b6c404a405acf167dec4fa5aca83b553f71
Hendrik Niemeyer <hendrik.niemeyer@gmail.com>
2021-12-10 07:08:56 +0100

    use normal clang version

--- build_memory.sh
+++ build_memory.sh
@@ -5,5 +5,5 @@ echo "Deleting previous build"
 rm -rf build/
 fi
 echo "Memory Sanitizer"
-cmake -S . -B ./build -GNinja -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_COMPILER=clang++-10 -DENABLE_SANITIZER_MEMORY=ON
+cmake -S . -B ./build -GNinja -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_COMPILER=clang -DENABLE_SANITIZER_MEMORY=ON
 cmake --build ./build -v
